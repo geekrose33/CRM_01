8a72e031b7a57ca7c59bfb812416d9523cc14a9a
geekrose <geekrose@qq.com>
2021-11-13 22:30:50 +0800

    测试模块

---
+++ src/test/java/com/geekrose/crm/test/UserTest.java
@@ -0,0 +1,16 @@
+package com.geekrose.crm.test;
+
+import org.junit.Test;
+
+/**
+ * @author Joker_Dong
+ * @date 2021-11-13 22:29
+ */
+
+public class UserTest {
+    @Test
+    public void testLogin(){
+
+
+    }
+}
